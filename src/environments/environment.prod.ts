c9848c8f0218dd0b43300e2c117cf585618e1bd3
Subin John <subinjohn.in@gmail.com>
2019-09-23 19:20:26 -0500

    Updating the service URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serviceUrl:'http://ec2-3-18-213-250.us-east-2.compute.amazonaws.com:3000/employee/'
+  serviceUrl:'http://ec2-18-219-185-32.us-east-2.compute.amazonaws.com:3000/employee/'
 };
